68cf25f39f5df190e7a0eaff128236c597431d91
crackalamoo <harysdalvi@gmail.com>
2025-04-12 21:13:58 -0400

    add jawi

--- aljamiado/convert.js
+++ aljamiado/convert.js
@@ -5,7 +5,11 @@ const outputLangs = {
     'ms': ['Jawi'],
 };
 
-const VOWELS = new Set(['a', 'e', 'i', 'o', 'u']);
+const VOWELS = new Set(['a', 'e', 'i', 'o', 'u',
+    'á', 'é', 'í', 'ó', 'ú',
+    'ĕ', 'ü',
+    'à', 'ä', 'ò',
+]);
 
 function contains(set, item) {
     return (new Set(set).has(item));
@@ -23,9 +27,12 @@ function isRoman(word) {
 function convertWords(text, mapper, nonWordMapper=null, noSpaceAfter=null) {
     let chars = [];
     let isWord = [];
+    let punct = [' ',',',':',';','.','!','?',';','\n','¿','¡'];
+    if (inputLang !== 'ms')
+        punct.push('-');
     for (let i = 0; i < text.length; i++) {
         chars.push(text[i]);
-        if (contains([' ',',',':',';','.','!','?',';','-','\n','¿','¡'], text[i])) {
+        if (contains(punct, text[i])) {
             isWord.push(false);
         } else {
             isWord.push(true);
@@ -81,6 +88,8 @@ function convertWords(text, mapper, nonWordMapper=null, noSpaceAfter=null) {
 function convertWord(startWord, inLang, outLang) {
     if (inLang === 'es') {
         return spanishAljamiado(properOrthography(startWord, inLang).toLowerCase());
+    } else if (inLang === 'ms') {
+        return malayJawi(properOrthography(startWord, inLang).toLowerCase());
     }
     return startWord;
 }
@@ -277,8 +286,7 @@ function spanishAljamiado(word) {
 
     word = word.replaceAll("'",'').replaceAll('´','').replaceAll('˜','');
 
-    const cons_set = new Set(Object.values(cons_map));
-    console.log(word);
+    const consSet = new Set(Object.values(cons_map));
     const diacritic_set = new Set(['َ','ِ','ُ','ّ','ٗ','ْ']);
     for (let i = word.length; i >= 0; i--) {
         let char = word.substring(i,i+1);
@@ -287,11 +295,194 @@ function spanishAljamiado(word) {
             next = '';
         else
             next = word.substring(i+1,i+2);
-        if ((cons_set.has(char) || char === 'ّ') && !diacritic_set.has(next)) {
+        if ((consSet.has(char) || char === 'ّ') && !diacritic_set.has(next)) {
             word = word.substring(0,i+1) + 'ْ' + word.substring(i+1);
         }
     }
     word = word.replaceAll('ٗ','');
+
+    return word;
+}
+
+function malayJawi(word) {
+    if (word === '-')
+        return word;
+    const predefined = {
+        'ada':'اد',
+        'di':'د',
+        'dia':'دي',
+        'dan':'دان',
+        'ia':'اي',
+        'jika':'جک',
+        'juga':'جوݢ',
+        'ke':'ک',
+        'lima':'ليم',
+        'kita':'كيت',
+        'mereka':'مريک',
+        'ini':'اين',
+        'itu':'ايت',
+        'pada':'ڤد',
+        'suka':'سوک',
+        'tiga':'تيݢ',
+    };
+    if (predefined[word] !== undefined) {
+        return predefined[word];
+    }
+
+    const hyphen = word.indexOf('-');
+    let vowelCount = 0;
+    for (let i = 0; i < (hyphen === -1 ? word.length : hyphen); i++) {
+        if (VOWELS.has(word.substring(i,i+1)))
+            vowelCount++;
+    }
+    if (hyphen !== -1) {
+        if (word.substring(0,hyphen) === word.substring(hyphen+1)) {
+            if (vowelCount <= 2) {
+                return malayJawi(word.substring(0,hyphen)) + '۲';
+            }
+        }
+        return malayJawi(word.substring(0,hyphen)) + '-' + malayJawi(word.substring(hyphen+1));
+    }
+    word = word.replaceAll('ĕ','e').replaceAll('x','Ks');
+    word = word.replaceAll('sy','š').replaceAll('sh','š');
+    word = word.replaceAll('ny','ñ').replaceAll('ng','ŋ').replaceAll('kh','X');
+    word = word.replaceAll('ai','à').replaceAll('au','ä').replaceAll('oi','ò');
+    word = word.replaceAll('o','u');
+
+    word = word.replaceAll('K','k').replaceAll('aa','a`').replaceAll('ua','u`a')
+    .replaceAll('ia','ì').replaceAll('ie','ì').replaceAll('iu','ìw').replaceAll('ii','ìy')
+    .replaceAll('ea','ì').replaceAll('ee','ì').replaceAll('ei','ìy').replaceAll('eu','ìw')
+    .replaceAll('éi','é`i').replaceAll('ié','i`é');
+    const closedSyllable = []
+    for (let i = 0; i < word.length; i++) {
+        let prev = word.substring(i-1,i);
+        let here = word.substring(i, i+1);
+        let next = word.substring(i+1, i+2);
+        let next2 = word.substring(i+2, i+3);
+        if (VOWELS.has(here) && (VOWELS.has(next) || VOWELS.has(next2) || prev === '`' || next === '`')) {
+            closedSyllable.push(false);
+        } else {
+            closedSyllable.push(true);
+        }
+    }
+    for (let i = 1; i < word.length; i++) {
+        let here = word.substring(i, i+1);
+        let prev = word.substring(i-1,i);
+        if (VOWELS.has(prev) && closedSyllable[i-1] && here === 'k')
+            word = word.substring(0,i) + 'q' + word.substring(i+1);
+    }
+    
+    if (word.startsWith("w") && vowelCount === 1 && VOWELS.has(word.substring(1,2))) {
+        closedSyllable[1] = false;
+    } else if (word.length === 5 && !VOWELS.has(word.substring(0,1))
+        && word.substring(1,4) === 'éwa' && !VOWELS.has(word.substring(4,5))) {
+        closedSyllable[3] = false;
+    }
+    
+    if (vowelCount >= 2 && word.match(/[^aeéiou]a[btpsgnñckjmy]a$/)) {
+        word = word.substring(0, word.length-1);
+    }
+    if (vowelCount >= 2 && word.match(/^[^aeéiou]aha/)) {
+        word = word.substring(0, 1) + 'e' + word.substring(2);
+    }
+
+    const cons_map = {
+        'b':'ب',
+        't':'ت',
+        'j':'ج',
+        'c':'چ',
+        'K':'خ',
+        'd':'د',
+        'r':'ر',
+        'z':'ز',
+        's':'س',
+        'š':'ش',
+        'ŋ':'ڠ',
+        'f':'ف',
+        'p':'ڤ',
+        'q':'ق',
+        'k':'ک',
+        'g':'ݢ',
+        'l':'ل',
+        'm':'م',
+        'n':'ن',
+        'w':'و',
+        'v':'ۏ',
+        'h':'ه',
+        'y':'ي',
+        'ñ':'ڽ',
+        'X':'خ',
+        '`':'ء'
+    };
+    const vow_map_1 = {
+        'a':'ا',
+        'é':'اي',
+        'e':'ا',
+        'i':'اي',
+        'u':'او',
+        'à':'اءي',
+        'ä':'اءو',
+        'ò':'اووي',
+        'ì':'أ'
+    };
+    const vow_map_open = {
+        'a':'ا',
+        'é':'ي',
+        'e':'',
+        'i':'ي',
+        'u':'و',
+        'à':'اي',
+        'ä':'او',
+        'ò':'وي',
+        'ì':'أ'
+    };
+    const vow_map_closed = {
+        'a':'',
+        'é':'ي',
+        'e':'',
+        'i':'ي',
+        'u':'و',
+        'à':'اءي',
+        'ä':'اءو',
+        'ò':'وءي',
+        'ì':'أ'
+    };
+    const vow_map_2 = {
+        'a':'ا',
+        'é':'ي',
+        'e':'ی',
+        'i':'ي',
+        'u':'و',
+        'à':'اي',
+        'ä':'او',
+        'ò':'وي',
+        'ì':'أ'
+    };
+
+    word = "'" + word + "'";
+    closedSyllable.unshift(false);
+    closedSyllable.push(false);
+
+    for (const [key, value] of Object.entries(cons_map)) {
+        word = word.replaceAll(key, value);
+    }
+    for (const [key, value] of Object.entries(vow_map_1)) {
+        word = word.replaceAll("'"+key, value);
+    }
+    for (const [key, value] of Object.entries(vow_map_2)) {
+        word = word.replaceAll(key+"'", value);
+    }
+    for (let i = word.length-2; i >= 0; i--) {
+        let char = word.substring(i,i+1);
+        if (!closedSyllable[i] && vow_map_open[char] !== undefined) {
+            word = word.substring(0,i) + vow_map_open[char] + word.substring(i+1);
+        } else if (closedSyllable[i] && vow_map_closed[char] !== undefined) {
+            word = word.substring(0,i) + vow_map_closed[char] + word.substring(i+1);
+        }
+    }
+    word = word.replaceAll('يءي', 'يئي');
+
+    word = word.replaceAll("'",'');
 
     return word;
 }
